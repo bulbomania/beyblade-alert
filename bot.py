841f4a2e94efd002c163dfa6de26154dc8ad63bc
bulbomania <grobberioale@gmail.com>
2026-06-20 13:35:03 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,79 @@
-import os
 import requests
+from bs4 import BeautifulSoup
+import json
+import os
 
 TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
-print("CHAT_ID =", CHAT_ID)
+URL = "https://www.amazon.co.jp/s?k=beyblade+x&crid=33LWC80LC0AK6&sprefix=%2Caps%2C232&ref=nb_sb_noss_2"
 
-r = requests.post(
-    f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-    json={
-        "chat_id": CHAT_ID,
-        "text": "✅ Test GitHub Actions"
-    }
+DB_FILE = "seen.json"
+
+def send_message(text):
+    r = requests.post(
+        f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+        json={
+            "chat_id": CHAT_ID,
+            "text": text
+        }
+    )
+
+    print("TELEGRAM STATUS:", r.status_code)
+
+def load_seen():
+    if os.path.exists(DB_FILE):
+        with open(DB_FILE) as f:
+            return set(json.load(f))
+    return set()
+
+def save_seen(data):
+    with open(DB_FILE, "w") as f:
+        json.dump(list(data), f)
+
+r = requests.get(
+    URL,
+    headers={"User-Agent": "Mozilla/5.0"}
 )
 
-print("STATUS:", r.status_code)
-print("RESPONSE:", r.text)
+print("AMAZON STATUS:", r.status_code)
+
+soup = BeautifulSoup(r.text, "html.parser")
+
+items = soup.select("[data-asin]")
+
+print("PRODOTTI TROVATI:", len(items))
+
+seen = load_seen()
+current = set()
+
+for item in items:
+    asin = item.get("data-asin", "").strip()
+
+    print("ASIN:", asin)
+
+    if not asin:
+        continue
+
+    title = item.select_one("h2 span")
+
+    if not title:
+        continue
+
+    title = title.get_text(strip=True)
+
+    print("TITOLO:", title)
+
+    current.add(asin)
+
+    if asin not in seen:
+        print("NUOVO PRODOTTO:", title)
+
+        send_message(
+            f"🆕 Nuovo prodotto Beyblade X:\n\n{title}"
+        )
+
+seen.update(current)
+save_seen(seen)
+
+print("FINE")
